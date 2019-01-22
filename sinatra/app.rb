ae3fd5058dd1182a0c6e71b3e5b2c6df37bc5ada
Jake Atkin <jakeatkin@Jakes-MacBook-Air.local>
2019-01-22 15:07:56 +0000

    Added aform & input values on the named cat page.

--- sinatra/app.rb
+++ sinatra/app.rb
@@ -6,12 +6,19 @@ get '/' do
   "Hello!"
 end
 
-  get '/cat' do
-    @random_name = ["Amigo", "Oscar", "Viking"].sample 
+  get '/random-cat' do
+    @random_name = ["Amigo", "Oscar", "Viking"].sample
     erb(:index)
   end
 
-
+  get '/named-cat' do
+    @random_name = params[:name]
+    erb(:index)
+  end
+#  params is a hash that Sinatra makes available for you in your route blocks, and it will automatically include
+   # relevant data from the request, i.e. if you do this request http://localhost:9393/named-cat?name=Bob on your
+   # browser, it will set the params[:name] where :name is from the ?name, to whatever you give it, in this case Bob.
+   # So the params ?name=Bob acts like a key/value pair of a hash.
 
 get '/secret' do
   'This is a secret page'
