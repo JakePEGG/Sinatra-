fef7908401fb82318464aa8fdb01077a806813f7
Jake Atkin <jakeatkin@Jakes-MacBook-Air.local>
2019-01-24 14:18:12 +0000

    Included sessions into my programme and used them across tests and .erbs'

--- Battle/app.rb
+++ Battle/app.rb
@@ -2,6 +2,7 @@ require 'sinatra/base'
 require 'shotgun'
 
 class Battle < Sinatra::Base
+  enable  :sessions
   get '/' do
     # 'Hello Battle!'
     erb :index
@@ -11,12 +12,24 @@ class Battle < Sinatra::Base
     erb :index
   end
 
+  get '/play' do
+    erb :play
+  end
+
   post '/names' do
-  @player_1_name = params[:player_1_name]
-  @player_2_name = params[:player_2_name]
-  erb :play
+  session[:player_1_name] = params[:player_1_name]
+  session[:player_2_name] = params[:player_2_name]
+  redirect '/play'
 end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
+
+
+
+
+
+
+  #.erb files have to be in view so they can be read just like Rspec.
+  # naming convention on Index that doesnt associate root (/) but has elements like /name that call on a route
